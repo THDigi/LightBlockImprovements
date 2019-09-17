81b127db95dbc01c542ac1760fd9eed1329aee1b
Digi <hunter_digital_sl@yahoo.com>
2019-09-17 23:21:12 +0300

    Restoring light definition data on unload

--- Data/Scripts/LightBlockImprovements/LightBlockImprovements.cs
+++ Data/Scripts/LightBlockImprovements/LightBlockImprovements.cs
@@ -9,6 +9,7 @@ using VRage.Game;
 using VRage.Game.Components;
 using VRage.Game.Entity;
 using VRage.Game.ModAPI;
+using VRage.ObjectBuilders;
 using VRage.Utils;
 using VRageMath;
 
@@ -21,6 +22,7 @@ namespace Digi.LightBlockImprovements
         private const string NEW_LIGHT_SUBID = "SmallBlockSmallLight"; // subtype ID to replace the above block with
 
         private readonly HashSet<IMySlimBlock> replaceBlocks = new HashSet<IMySlimBlock>();
+        private readonly Dictionary<MyDefinitionId, BackupDefData> defDataBackups = new Dictionary<MyDefinitionId, BackupDefData>(MyDefinitionId.Comparer);
 
         public override void LoadData()
         {
@@ -34,31 +36,7 @@ namespace Digi.LightBlockImprovements
                     MyEntities.OnEntityAdd += OnEntityAdded;
                 }
 
-                // Dynamic definition editing without needing to set the other values
-                MyLightingBlockDefinition def;
-
-                def = GetDefAndSetModel(typeof(MyObjectBuilder_InteriorLight), "SmallLight", @"Models\LargeInteriorLight.mwm");
-                if(def != null)
-                {
-                    def.LightFalloff.Min = 0.5f;
-                    def.LightIntensity.Min = 0.5f;
-                }
-
-                def = GetDefAndSetModel(typeof(MyObjectBuilder_InteriorLight), "SmallBlockSmallLight", @"Models\SmallInteriorLight.mwm");
-                if(def != null)
-                {
-                    def.LightFalloff.Min = 0.5f;
-                    def.LightIntensity.Min = 0.5f;
-                }
-
-                def = GetDefAndSetModel(typeof(MyObjectBuilder_ReflectorLight), "LargeBlockFrontLight", @"Models\LargeSpotlight.mwm");
-                def = GetDefAndSetModel(typeof(MyObjectBuilder_ReflectorLight), "SmallBlockFrontLight", @"Models\SmallSpotlight.mwm");
-
-                def = GetDefAndSetModel(typeof(MyObjectBuilder_InteriorLight), "LargeBlockLight_1corner", @"Models\LargeCornerLight.mwm");
-                def = GetDefAndSetModel(typeof(MyObjectBuilder_InteriorLight), "SmallBlockLight_1corner", @"Models\SmallCornerLight.mwm");
-
-                def = GetDefAndSetModel(typeof(MyObjectBuilder_InteriorLight), "LargeBlockLight_2corner", @"Models\LargeCornerLightDouble.mwm");
-                def = GetDefAndSetModel(typeof(MyObjectBuilder_InteriorLight), "SmallBlockLight_2corner", @"Models\SmallCornerLightDouble.mwm");
+                EditDefinitions();
             }
             catch(Exception e)
             {
@@ -72,26 +50,17 @@ namespace Digi.LightBlockImprovements
         {
             try
             {
-                MyEntities.OnEntityAdd -= OnEntityAdded;
+                if(MyAPIGateway.Session.IsServer)
+                {
+                    MyEntities.OnEntityAdd -= OnEntityAdded;
+                }
+
+                RestoreVanillaDefinitions();
             }
             finally
             {
                 Log.Close();
             }
-        }
-
-        private MyLightingBlockDefinition GetDefAndSetModel(Type type, string subtypeId, string model)
-        {
-            MyCubeBlockDefinition def;
-
-            if(MyDefinitionManager.Static.TryGetCubeBlockDefinition(new MyDefinitionId(type, MyStringHash.GetOrCompute(subtypeId)), out def))
-            {
-                var lightDef = (MyLightingBlockDefinition)def;
-                lightDef.Model = Path.Combine(ModContext.ModPath, model);
-                return lightDef;
-            }
-
-            return null;
         }
 
         // Used for replacing a block in every grid that spawns; must be only server side
@@ -135,5 +104,102 @@ namespace Digi.LightBlockImprovements
                 MyAPIGateway.Utilities.ShowNotification($"[ Error in {GetType().FullName}: {e.Message} ]", 10000, MyFontEnum.Red);
             }
         }
+
+        #region Definition editing
+        private void EditDefinitions()
+        {
+            SimpleModify(typeof(MyObjectBuilder_InteriorLight), "SmallLight", @"Models\LargeInteriorLight.mwm", falloff_min: 0.5f, intensity_min: 0.5f);
+            SimpleModify(typeof(MyObjectBuilder_InteriorLight), "SmallBlockSmallLight", @"Models\SmallInteriorLight.mwm", falloff_min: 0.5f, intensity_min: 0.5f);
+            SimpleModify(typeof(MyObjectBuilder_ReflectorLight), "LargeBlockFrontLight", @"Models\LargeSpotlight.mwm");
+            SimpleModify(typeof(MyObjectBuilder_ReflectorLight), "SmallBlockFrontLight", @"Models\SmallSpotlight.mwm");
+            SimpleModify(typeof(MyObjectBuilder_InteriorLight), "LargeBlockLight_1corner", @"Models\LargeCornerLight.mwm");
+            SimpleModify(typeof(MyObjectBuilder_InteriorLight), "SmallBlockLight_1corner", @"Models\SmallCornerLight.mwm");
+            SimpleModify(typeof(MyObjectBuilder_InteriorLight), "LargeBlockLight_2corner", @"Models\LargeCornerLightDouble.mwm");
+            SimpleModify(typeof(MyObjectBuilder_InteriorLight), "SmallBlockLight_2corner", @"Models\SmallCornerLightDouble.mwm");
+        }
+
+        private void SimpleModify(MyObjectBuilderType obType, string subType, string modelRelativePath, float? falloff_min = null, float? intensity_min = null)
+        {
+            var lightDef = GetLightDefById(obType, subType);
+
+            if(lightDef == null)
+            {
+                Log.Error($"Couldn't find def: {obType.ToString()}/{subType}");
+                return;
+            }
+
+            var backup = new BackupDefData();
+
+            if(modelRelativePath != null)
+            {
+                backup.Model = lightDef.Model;
+                lightDef.Model = Path.Combine(ModContext.ModPath, modelRelativePath);
+            }
+
+            if(falloff_min.HasValue)
+            {
+                backup.LightFalloff = lightDef.LightFalloff; // MyBounds is struct, gets copied
+                lightDef.LightFalloff.Min = falloff_min.Value;
+            }
+
+            if(intensity_min.HasValue)
+            {
+                backup.LightIntensity = lightDef.LightIntensity;
+                lightDef.LightIntensity.Min = intensity_min.Value;
+            }
+
+            AddDefBackup(lightDef.Id, backup);
+        }
+
+        private void RestoreVanillaDefinitions()
+        {
+            if(defDataBackups == null)
+                return;
+
+            foreach(var kv in defDataBackups)
+            {
+                var id = kv.Key;
+                var lightDef = GetLightDefById(id.TypeId, id.SubtypeName);
+
+                if(lightDef == null)
+                    continue; // shouldn't be possible but at this point who cares
+
+                var backup = kv.Value;
+
+                if(backup.Model != null)
+                    lightDef.Model = backup.Model;
+
+                if(backup.LightFalloff.HasValue)
+                    lightDef.LightFalloff = backup.LightFalloff.Value;
+
+                if(backup.LightIntensity.HasValue)
+                    lightDef.LightIntensity = backup.LightIntensity.Value;
+            }
+
+            defDataBackups.Clear();
+        }
+
+        private MyLightingBlockDefinition GetLightDefById(MyObjectBuilderType type, string subtypeId)
+        {
+            MyCubeBlockDefinition def;
+
+            if(MyDefinitionManager.Static.TryGetCubeBlockDefinition(new MyDefinitionId(type, MyStringHash.GetOrCompute(subtypeId)), out def))
+                return (MyLightingBlockDefinition)def;
+
+            return null;
+        }
+
+        private void AddDefBackup(MyDefinitionId defId, BackupDefData data)
+        {
+            defDataBackups.Add(defId, data);
+        }
+
+        private class BackupDefData
+        {
+            public string Model;
+            public MyBounds? LightFalloff;
+            public MyBounds? LightIntensity;
+        }
+        #endregion Definition editing
     }
 }
